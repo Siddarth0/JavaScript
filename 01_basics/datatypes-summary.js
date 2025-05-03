82890e235f9880ecc80809c4d0463cbf757f97ea
Siddartha Kunwar <109203791+Siddarth0@users.noreply.github.com>
2025-05-03 06:18:37 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes =["ironman", "doga", "power ranger"]
+let myObj = {
+    name: "siddarth",
+    age: 24,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof myFunction)
